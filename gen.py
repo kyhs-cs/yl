7e72171498509a5ed29a7e1ff4758df1d895bd68
Rotem Moses <rotem@faceco.de>
2022-01-20 14:51:25 +0200

    Initial commit

---
+++ gen.py
@@ -0,0 +1,19 @@
+urls = ['https://www.youtube.com/watch?v=FepDt4KniyQ', 'https://www.youtube.com/watch?v=vsB5iKhFx4g', 'https://example.com', 'https://www.youtube.com/watch?v=9ZGfH0R_tb4', 'https://www.youtube.com/watch?v=Z4vtJTQQojw', 'https://www.youtube.com/watch?v=-bdgTfu-ry4']
+
+for i in range(len(urls)):
+    file = open(f'{i}.html', 'w')
+    content = f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <meta http-equiv="refresh" content="0; URL={urls[i]}">
+      <meta charset="UTF-8">
+      <title>Redirecting...</title>
+    </head>
+    <body>
+      <p>If you are not redirected, <a href="{urls[i]}">click here</a>.</p>
+    </body>
+    </html>
+    """
+    file.write(content)
+    file.close()
